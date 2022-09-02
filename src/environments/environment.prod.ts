0d1bfcec853e78978fd83ac20ea2537e2570af15
Kevin Loubser <zeptin@gmail.com>
2022-09-02 17:20:22 +0200

    Testnet API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ import { ChainName } from 'src/app/services/chain';
 
 export const environment: Environment = {
   production: true,
-  apiEndpoint: 'https://interflux-rates.stratisplatform.com',
-  wstraxaddress: '0xa3c22370de5f9544f0c4de126b1e46ceadf0a51b',
-  wstraxaddressprefix: 75,
-  chain: ChainName.Main,
+  apiEndpoint: 'https://cirrushelperapi-testnet.azurewebsites.net',
+  wstraxaddress: '0xde09a7cb4c7631f243e5a5454cbb02404aea65e7',
+  wstraxaddressprefix: 120,
+  chain: ChainName.Ropsten,
   alchemyApiKey: 'demo'
 };
