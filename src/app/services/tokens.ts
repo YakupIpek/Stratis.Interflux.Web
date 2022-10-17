2c5f53de0c06f10f2f2c90499969225d6d8a7795
Francois de la Rouviere <fassadlr@gmail.com>
2022-10-17 14:09:29 +0100

    Update tokens.ts

--- src/app/services/tokens.ts
+++ src/app/services/tokens.ts
@@ -102,63 +102,63 @@ export interface TokenData {
 export const TOKENS: TokenData[] = [
   {
     ticker: 'WSTRAX',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'WStrax => Strax',
-    destination: 'Strax',
+    destination: Destination.Strax,
     erc20: '0xa3c22370de5f9544f0c4de126b1e46ceadf0a51b',
     addressPrefix: 75,
     decimals: 18
   },
   {
     ticker: 'WETH',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'Wrapped ETH',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     addressPrefix: 28,
     decimals: 18
   },
   {
     ticker: 'WBTC',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'Wrapped BTC',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
     addressPrefix: 28,
     decimals: 8
   },
   {
     ticker: 'USDC',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'USDC',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     addressPrefix: 28,
     decimals: 6
   },
   {
     ticker: 'USDT',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'USDT',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
     addressPrefix: 28,
     decimals: 6
   },
   {
     ticker: 'LINK',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'LINK',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
     addressPrefix: 28,
     decimals: 18
   },
   {
     ticker: 'SHIB',
-    chain: 'Main',
+    chain: ChainName.Main,
     title: 'SHIB',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE',
     addressPrefix: 28,
     decimals: 18
@@ -167,36 +167,36 @@ export const TOKENS: TokenData[] = [
   //
   {
     ticker: 'WSTRAX',
-    chain: 'Sepolia',
+    chain: ChainName.Sepolia,
     title: 'WStrax => Strax',
-    destination: 'Strax',
+    destination: Destination.Strax,
     erc20: '0xf128715b5AF562b2B45BBc8ede79973Ff48C3815',
     addressPrefix: 120,
     decimals: 18
   },
   {
     ticker: 'TSZ1',
-    chain: 'Sepolia',
+    chain: ChainName.Sepolia,
     title: 'Test Token Z1',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0xE4a444CB3222fd8E9518dB8F70A33aaDb9a1a358',
     addressPrefix: 127,
     decimals: 18
   },
   {
     ticker: 'TSZ2',
-    chain: 'Sepolia',
+    chain: ChainName.Sepolia,
     title: 'Test Token Z2',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0xF197f5f8c406d269E2cc44Aaf495fBC4EB519634',
     addressPrefix: 127,
     decimals: 8
   },
   {
     ticker: 'TSZ3',
-    chain: 'Sepolia',
+    chain: ChainName.Sepolia,
     title: 'Test Token Z3',
-    destination: 'Cirrus',
+    destination: Destination.Cirrus,
     erc20: '0xa3C22370de5f9544f0c4De126b1e46cEadF0A51B',
     addressPrefix: 127,
     decimals: 6
