f8e341d2c3d0d00816a4560e1e1ae15cde3ac47d
Francois de la Rouviere <fassadlr@gmail.com>
2022-10-17 12:04:44 +0100

    Updates relating to Sepolia

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ import { ChainName } from 'src/app/services/chain';
 export const environment: Environment = {
   production: true,
   apiEndpoint: 'https://cirrushelperapi-testnet.azurewebsites.net',
-  wstraxaddress: '0xde09a7cb4c7631f243e5a5454cbb02404aea65e7',
+  wstraxaddress: '0xf128715b5AF562b2B45BBc8ede79973Ff48C3815',
   wstraxaddressprefix: 120,
-  chain: ChainName.Ropsten,
+  chain: ChainName.Sepolia,
   alchemyApiKey: 'demo'
 };
